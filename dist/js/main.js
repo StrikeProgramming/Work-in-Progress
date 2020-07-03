e5cc650596ac688ab8ca14b14013f744586c8665
brad <bradawroberts@gmail.com>
2020-07-03 15:15:39 +0100

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,14 @@
+/* When page is scrolled on header */
+
+const header = document.querySelector('.main-header');
+
+window.addEventListener('scroll', () => {
+    const scrollPos = window.scrollY;
+    if(scrollPos > 10){
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+})
+
+
